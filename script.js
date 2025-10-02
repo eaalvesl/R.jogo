feee619296356195f6c13df24f4dd8abcc17293b
eaalvesl <rafaella.alves.rosa@escola.pr.gov.br>
2025-10-02 17:13:54 -0300

    oi

--- script.js
+++ script.js
@@ -30,7 +30,95 @@ const charadas = [
   {
     pergunta: "O que é invisível e se você fala, desaparece?",
     resposta: "silêncio"
-  }
+  },
+  {
+    pergunta: "É seu, mas as outras pessoas usam mais do que você?",
+    resposta: "Seu nome"
+  },
+  {
+    pergunta: "Fica molhado enquanto seca os outros?",
+    resposta: "A toalha"
+  },
+  {
+    pergunta: "Tem cidade, mas não tem casas?",
+    resposta: "O mapa"
+  },
+  {
+    pergunta: "Você quebra antes mesmo de usar?",
+    resposta: "Um ovo"
+  },
+  {
+    pergunta: "Tem coroa, mas não é rei, tem escamas mas não é peixe?",
+    resposta: "O abacaxi"
+  },
+  {
+    pergunta: "É maior que um elefante, mas não pesa nada?",
+    resposta: "A sombra do elefante"
+  },
+  {
+    pergunta: "Tem folhas mas não é árvore, Conta histórias mas não fala"
+    resposta: "O livro"
+  },
+  {
+    pergunta: "Tem pernas, mas não anda?"
+    resposta: "A cadeira"
+  },
+  {
+    pergunta: "Anda sem sair do lugar?"
+    resposta: "O relógio"
+  },
+  {
+    pergunta: "Quanto mais enche, mais leve fica?"
+    resposta: "O balão"
+  },
+  {
+    pergunta: "Tem língua mas não fala, tem boca mas não come?"
+    resposta: "O sapato"
+  },
+{
+    pergunta: "Cai em pé e corre deitado?"
+    resposta: "A chuva"
+  },
+  {
+    pergunta: "Tem chave, mas não abre porta?"
+    resposta: "O teclado"
+  },
+  {
+    pergunta: "Sempre olha para você, mas nunca fala?"
+    resposta: "O espelho"
+  },
+  {
+    pergunta: "Sou cheio de furos, mas consigo reter água?"
+    resposta: "A esponja"
+  },
+  {
+    pergunta: "Não tem asas mas voa, não tem olhos mas chora"
+    resposta: "A nuvem"
+  },
+  {
+    pergunta: "Sou comprado para comer, mas nunca como?"
+    resposta: "O prato"
+  },
+  {
+    pergunta: "Quanto mais se espalha, menos se vê?"
+    resposta: "A neblina"
+  },
+   {
+    pergunta: "Quanto mais rápido você corre, mais ele se afasta?"
+    resposta: "O horizonte"
+  },
+   {
+    pergunta: "Não tem asas, mas pode voar com o vento?"
+    resposta: "O papel"
+  },
+   {
+    pergunta: "Nasce grande e morre pequeno?"
+    resposta: "O lápis"
+  },
+   {
+    pergunta: "Tem um pescoço, mas não tem cabeça"
+    resposta: "A garrafa"
+  },
 ];
 
 let indiceAtual = 0;
